9d1462b1db302b52e7a3fc840865867a071559c9
dasolma <dasolma@gmail.com>
2014-05-23 15:22:54 +0200

    version 1 LVCS

--- image/processor.py
+++ image/processor.py
@@ -4,12 +4,25 @@ from PIL import ImageFont
 from PIL import ImageDraw
 
 
-def addtext(text, image, cols, font_size = 16):
-    rows = int(len(text) / cols) + 1
-    black = (0,0,0)
+def addtext(text, image, cols, font_size = 11, rows = None):
+    red = (255,0,0,255)
+    green = (0,255,0,255)
+    blue = (0,0,255,255)
+    black = (0,0,0,255)
+    colors = [red,green,blue,black]
+    if rows == None:
+        rows = int(len(text) / cols) + 1
+
+    #load font
+    font = ImageFont.truetype("DejaVuSans.ttf", font_size)
+
+    #reduce space between letters
+    font_size -= int(font_size * 0.2)
+
+
     if image is None:
         # create image
-        image = createimage(rows, cols, font_size)
+        image = createimage(rows*len(text[0]), cols*len(text[0][0]), font_size)
 
     else:
         # check the size
@@ -17,32 +30,56 @@ def addtext(text, image, cols, font_size = 16):
         (w, h) = image.size
 
         #canvas resizing if it is neccesary
-        print (w, h)
-        print (wn, hn)
+        #print (w, h)
+        #print (wn, hn)
         if w < wn or h < hn:
             img = createimage(rows, cols, font_size)
             img.paste(image, (0, 0, w, h))
             image = img
 
-    #load font
-    font = ImageFont.truetype("DejaVuSans.ttf", font_size)
+
 
     #draw object to write the text
     draw = ImageDraw.Draw(image)
     index = 0
 
+    total = 0
 
+    rf = 0
     for r in range(0,rows):
+        cf = 0
         for c in range(0,cols):
-            char = text[index]
+            superpixel = text[index]
+            #print superpixel
+            if len(superpixel[0]) > 1:
+                srf = 0
+                for rsp in superpixel:
 
-            draw.text((c * font_size, r * font_size),char,black,font=font)
+
+                    for char in rsp:
+                        draw.text((cf * (font_size),
+                                  (rf + srf) * (font_size)),
+                                   char, fill=black, font=font)
+
+                        cf += 1
+                    srf += 1
+                    cf -= len(rsp)
+                    total += 1
+
+                cf += len(superpixel[0])
+
+            else:
+                draw.text((c * font_size, r * font_size),superpixel,black,font=font)
+
+
             index = index + 1
-
+            #print index
             if( index >= len(text) ):
                 return image
 
+        rf += len(text[0])
 
+    del draw
     return image
 
 def overlaping(img1, img2, channel=0, thresold=125):
@@ -72,4 +109,4 @@ def createimage(rows, cols, font_size = 16):
 
     # create a new image surface
     # make the background white (default bg=black)
-    return Image.new("RGB", [cols * font_size, (rows + 1) * font_size], white)
+    return Image.new("RGB", (cols * font_size, (rows + 1) * font_size), white)
--- image/test.py
+++ image/test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from processor import *
 
+
 img1 = addtext('EstoesuntextoEstoesuntextoEstoesuntextoEstoesuntextoEstoesuntextoEstoesuntexto', None, 5)
 img1.save('test1.jpg')
 
@@ -12,3 +13,4 @@ img3.save('test3.jpg')
 
 img4 = overlaping(img1, img2)
 img4.save('test4.jpg')
+
